1ade655abbc8b3f98321df55ce603a1efd05e6ae
LazarevaL <l.lazareva@edu.misis.ru>
2024-05-30 22:30:39 +0300

    fix

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from setuptools import setup
 
 __version__: str = "1.0"
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-
 setup(
     name="OKX_news_parser",
     version=__version__,
-    install_requires=install_requires,
+    install_requires = ['beautifulsoup4', 'requests'],
 ), 
 url = 'ttps://github.com/LazarevaL/OKX_news_parser',
 download_url = 'https://github.com/LazarevaL/OKX_news_parser/tree/main.zip'
