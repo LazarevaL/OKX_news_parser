7c9d6052c49ff60f3d14c778cac0615960a1c64b
LazarevaL <l.lazareva@edu.misis.ru>
2024-05-30 22:42:03 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup , find_packages
 
-__version__: str = "1.0"
+__version__: str = "1.0.0"
 
 setup(
     name="OKX_news_parser",
     version=__version__,
-    install_requires = ['beautifulsoup4', 'requests'],
-), 
-url = 'ttps://github.com/LazarevaL/OKX_news_parser',
-download_url = 'https://github.com/LazarevaL/OKX_news_parser/tree/main.zip'
+    packages=find_packages(),
+    # python_requires=">=3.10.0",
+    install_requires=["beautifulsoup4==4.9.3", "requests==2.32.3"],
+)
